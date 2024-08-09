30130ca1ff250b9c6a4c4befc67acad93219f69c
Andrew Tamburino <andytamburino@gmail.com>
2024-08-08 23:52:51 -0400

    Added Todo: for tabs

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,6 +4,8 @@ function Header() {
       <li className="text-xs text-gray-800 ml-auto mr-6 border-b-2 border-green-400 cursor-pointer">
         Weather
       </li>
+
+      {/* TODO: Hook these tabs up one day */}
       {/* <li className="text-xs text-gray-800 mr-6 cursor-pointer border-b-2 hover:border-green-400">
         Alerts
       </li>
